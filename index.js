ade13536438ad51801bf0e603448ee6908baac3a
awsome-cats <cattwin82@gmail.com>
2021-05-29 23:40:04 +0900

    add config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const indexRoutes = require('./routes/books')
-const config = require('./config')
+const config = require('./config/config')
 
 
 const db = mysql.createConnection(config)
