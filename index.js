493eb404772de46c28a56242f44fda2207a663e0
awsome-cats <cattwin82@gmail.com>
2021-05-29 19:27:16 +0900

    update app

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT ||5000
 
 app.get('/', (req, res) => {
-    res.send('First node appへようこそ')
+    res.send('First node appへようこそ これからmysqlを導入したい')
 })
 
 
